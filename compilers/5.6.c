2f6cc14af8bed1db28b8d360979f1982e2a43b37
linkkingjay <linkkingjay@gmail.com>
2013-10-11 13:38:46 +0800

    update 5.6.c

--- compilers/5.6.c
+++ compilers/5.6.c
@@ -1,19 +1,35 @@
+/**
+ * S -> aAaB|bAbB
+ * A -> S|db
+ * B -> bB|a
+ * 
+ * 递归下降分析程序。
+ */
+
 void S()
 {
     if (sym == 'a' || sym == 'b') {
         getsym();
         A();
+        if (sym == 'a' || sym == 'b') {
+            getsym();
+            B();
+        } else {
+            error();
+        }
     } else {
         error();
     }
 }
 void A()
 {
-    if (sym == 'S')
+    if (sym == 'd') {
+        getsym();
+    } else if (sym == 'a' || 'b') {
         S();
-    else if (sym == 'd')
-    else
+    } else {
         error();
+    }
 }
 void B()
 {
@@ -21,6 +37,7 @@ void B()
         getsym();
         B();
     } else if (sym == 'a') {
+        getsym();
     } else {
         error();
     }
