903f4583af5e663e3044c801639bac01f0f644b0
linkkingjay <linkkingjay@gmail.com>
2014-12-09 09:26:43 +0800

    add chapter 1

---
+++ haskell/cp1.hs
@@ -0,0 +1,19 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x * 2
+
+doubleSmallNumber' x = (if x > 100
+                        then x
+                        else x * 2) + 1
+
+conanO'Brien = "Its a-me, Conan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
