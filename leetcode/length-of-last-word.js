37a2ad048ca0096e91e6902a5839d0bbc2bbd403
linkkingjay <linkkingjay@gmail.com>
2015-08-14 17:22:43 +0800

    add sols

---
+++ leetcode/length-of-last-word.js
@@ -0,0 +1,23 @@
+//Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+//If the last word does not exist, return 0.
+
+//Note: A word is defined as a character sequence consists of non-space characters only.
+
+//For example, 
+//Given s = "Hello World",
+//return 5.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  s = s.trim();
+  var lastIndex = s.lastIndexOf(' ');
+  if (lastIndex === -1) {
+    return s.length;
+  } else {
+    return s.length - lastIndex - 1;
+  }
+};
---
+++ leetcode/valid-anagram.js
@@ -0,0 +1,18 @@
+//Given two strings s and t, write a function to determine if t is an anagram of s.
+
+//For example,
+//s = "anagram", t = "nagaram", return true.
+//s = "rat", t = "car", return false.
+
+//Note:
+//You may assume the string contains only lowercase alphabets.
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  return s.split('').sort().join() === t.split('').sort().join();
+};
+
