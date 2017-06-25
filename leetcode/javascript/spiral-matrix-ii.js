5882cb64d9be9dd3f9a3722361116679cd016e56
linkqjlin <linkqjlin@tencent.com>
2017-06-25 22:43:55 +0800

    add leetcode
    1. spiral-matrix-ii.js
    2. wiggle-sort-ii.js

---
+++ leetcode/javascript/spiral-matrix-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+
+var generateMatrix = function(n) {
+  var dir = [
+    right: {x: 1, y: 0},
+    down: {x: 0, y: 1},
+    left: {x: -1, y: 0},
+    up: {x: 0, y: -1}
+  ]
+
+  var ret = []
+  var i, j
+
+  for (i = 0; i < n; i++) {
+    ret[i] = []
+    for (j = 0; j < n; j++) {
+      ret[i][j] = new
+    }
+  }
+
+  i = 0
+  j = 0
+  while (true) {
+    if ()
+  }
+
+  return ret
+};
+
+console.log(generateMatrix(3))
---
+++ leetcode/javascript/wiggle-sort-ii.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var wiggleSort = function(nums) {
+};
+
+var arr =  [1,5,1,1,6,4]
+wiggleSort(arr)
+console.log(arr)
